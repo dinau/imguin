ad76d166f8fdab946e6748fe915ca09ff7d7541c
dinau <audgit@gmail.com>
2023-06-17 18:44:14 +0900

    Show: DLL/SO info

--- examples/makefile.common.mk
+++ examples/makefile.common.mk
@@ -6,13 +6,15 @@ endif
 TARGET_EXE = $(TARGET)$(EXE)
 
 .PHONY: clean run dll ver gitup info danger release futhark \
-	      dsize rsize release
+	      dsize rsize release build
 
 OPT += -d:strip
 OPT += -o:$(TARGET_EXE)
 #OPT += --verbosity:2
 
-all:
+all: build dll
+
+build:
 	nim c $(OPT) $(IMPLOT) $(TARGET)
 
 danger:
@@ -41,10 +43,16 @@ info: dll ver
 dll:
 	@echo
 	@echo [dlls depend on]
+ifeq ($(OS),Windows_NT)
 	@-strings $(TARGET_EXE) |  rg -i \.dll
 	@echo
 	@#echo [cimgui.dll version]
 	@#-strings cimgui.dll | rg -ie "^\d\.\d\d\.\d"
+else
+	@-strings $(TARGET_EXE) |  rg -e "\.so" | sort | uniq
+	@echo
+endif
+
 ver:
 	@# version check
 	@echo [$(TARGET).nimlbe]
