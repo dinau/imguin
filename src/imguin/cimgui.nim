309034893826580dfdfd1516d3cfc6fb5acd6be1
System64 <42580501+system64MC@users.noreply.github.com>
2024-12-11 23:55:45 +0100

    cimgui.nim : Fixed cross-compilation issues

--- src/imguin/cimgui.nim
+++ src/imguin/cimgui.nim
@@ -12,10 +12,10 @@ const ClangIncludePath = "c:/drvDx/msys64/ucrt64/lib/clang/18/include"
 #const ClangIncludePath = "c:/llvm/lib/clang/17/include"
 
 # Set root path of ImGui/CImGui
-const CImguiRootPath    = joinPath(currentSourceDir(),"private/cimgui")
-const CImPlotRootPath   = joinPath(currentSourceDir(),"private/cimplot")
-const CImNodesRootPath  = joinPath(currentSourceDir(),"private/cimnodes")
-const CImGuizmoRootPath = joinPath(currentSourceDir(),"private/cimguizmo")
+const CImguiRootPath    = joinPath(currentSourceDir(),"private/cimgui").replace("\\", "/")
+const CImPlotRootPath   = joinPath(currentSourceDir(),"private/cimplot").replace("\\", "/")
+const CImNodesRootPath  = joinPath(currentSourceDir(),"private/cimnodes").replace("\\", "/")
+const CImGuizmoRootPath = joinPath(currentSourceDir(),"private/cimguizmo").replace("\\", "/")
 
 #--- Futhark start
 when defined(useFuthark): # Generate header files with Futhark.
@@ -76,31 +76,31 @@ else: # Use generated header by Futark in your programs.
   {.passC:"-I" & CImguiRootPath.}
   {.passC:"-I" & ImguiRootPath.}
 
-  {.compile:joinPath(CImguiRootPath,"cimgui.cpp").}
-  {.compile:joinPath(ImguiRootPath,"imgui.cpp").}
-  {.compile:joinPath(ImguiRootPath,"imgui_demo.cpp").}
-  {.compile:joinPath(ImguiRootPath,"imgui_draw.cpp").}
-  {.compile:joinPath(ImguiRootPath,"imgui_tables.cpp").}
-  {.compile:joinPath(ImguiRootPath,"imgui_widgets.cpp").}
+  {.compile:joinPath(CImguiRootPath,"cimgui.cpp").replace("\\", "/").}
+  {.compile:joinPath(ImguiRootPath,"imgui.cpp").replace("\\", "/").}
+  {.compile:joinPath(ImguiRootPath,"imgui_demo.cpp").replace("\\", "/").}
+  {.compile:joinPath(ImguiRootPath,"imgui_draw.cpp").replace("\\", "/").}
+  {.compile:joinPath(ImguiRootPath,"imgui_tables.cpp").replace("\\", "/").}
+  {.compile:joinPath(ImguiRootPath,"imgui_widgets.cpp").replace("\\", "/").}
 
   when defined(ImPlotEnable):
     {.passC:"-I" & CImPlotRootPath.}
-    {.compile:joinPath(CImPlotRootPath,"cimplot.cpp").}
-    {.compile:joinPath(CImPlotRootPath,"implot/implot.cpp").}
-    {.compile:joinPath(CImPlotRootPath,"implot/implot_demo.cpp").}
-    {.compile:joinPath(CImPlotRootPath,"implot/implot_items.cpp").}
+    {.compile:joinPath(CImPlotRootPath,"cimplot.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImPlotRootPath,"implot/implot.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImPlotRootPath,"implot/implot_demo.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImPlotRootPath,"implot/implot_items.cpp").replace("\\", "/").}
 
   when defined(ImNodesEnable):
     {.passC:"-DIMNODES_NAMESPACE=imnodes".}
     {.passC:"-I" & CImNodesRootPath.}
-    {.compile:joinPath(CImNodesRootPath,"cimnodes.cpp").}
-    {.compile:joinPath(CImNodesRootPath,"imnodes/imnodes.cpp").}
+    {.compile:joinPath(CImNodesRootPath,"cimnodes.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImNodesRootPath,"imnodes/imnodes.cpp").replace("\\", "/").}
 
   when defined(ImGuizmoEnable):
-    {.passC:"-I" &     CImGuizmoRootPath.}
-    {.compile:joinPath(CImGuizmoRootPath,"cimguizmo.cpp").}
-    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/GraphEditor.cpp").}
-    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/ImCurveEdit.cpp").}
-    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/ImGradient.cpp").}
-    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/ImGuizmo.cpp").}
-    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/ImSequencer.cpp").}
+    {.passC:"-I" & CImGuizmoRootPath.}
+    {.compile:joinPath(CImGuizmoRootPath,"cimguizmo.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/GraphEditor.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/ImCurveEdit.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/ImGradient.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/ImGuizmo.cpp").replace("\\", "/").}
+    {.compile:joinPath(CImGuizmoRootPath,"ImGuizmo/ImSequencer.cpp").replace("\\", "/").}
